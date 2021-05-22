9051b0cf5c4ace72e10811cae7f6dea46a31cd8c
HAKIM2000-JE <abdelhakimjebabra7@gmail.com>
2021-05-22 17:12:57 +0100

    first commit

---
+++ Migrations/20210522134105_StageModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity1.Migrations
+{
+    public partial class StageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stage",
+                columns: table => new
+                {
+                    StageID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    titre = table.Column<string>(nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    debut = table.Column<DateTime>(nullable: false),
+                    fin = table.Column<DateTime>(nullable: false),
+                    etat = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stage", x => x.StageID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stage");
+        }
+    }
+}
---
+++ Models/Stage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity1.Models
+{
+    public class Stage
+    {
+
+        public int StageID { get; set; }
+        public string titre { get; set; }
+        public string description { get; set; }
+        public DateTime debut { get; set; }
+        public DateTime fin { get; set; }
+        public string etat { get; set; }
+    }
+}
