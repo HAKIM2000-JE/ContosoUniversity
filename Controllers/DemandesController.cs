d9f6cdfe28a8b3c84ed56dabc281b9b56f65f3f6
Jebabra Abdelhakim <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-05-23 22:33:20 +0100

    Add files via upload

---
+++ Controllers/DemandesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity1.Data;
+using ContosoUniversity1.Models;
+
+namespace ContosoUniversity1.Controllers
+{
+    public class DemandesController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public DemandesController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Demandes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Demandes.ToListAsync());
+        }
+
+        // GET: Demandes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var demande = await _context.Demandes
+                .FirstOrDefaultAsync(m => m.DemandeID == id);
+            if (demande == null)
+            {
+                return NotFound();
+            }
+
+            return View(demande);
+        }
+
+        // GET: Demandes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Demandes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DemandeID,ID_stage,ID_student,etat")] Demande demande)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(demande);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(demande);
+        }
+
+        // GET: Demandes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var demande = await _context.Demandes.FindAsync(id);
+            if (demande == null)
+            {
+                return NotFound();
+            }
+            return View(demande);
+        }
+
+        // POST: Demandes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DemandeID,ID_stage,ID_student,etat")] Demande demande)
+        {
+            if (id != demande.DemandeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(demande);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DemandeExists(demande.DemandeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(demande);
+        }
+
+        // GET: Demandes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var demande = await _context.Demandes
+                .FirstOrDefaultAsync(m => m.DemandeID == id);
+            if (demande == null)
+            {
+                return NotFound();
+            }
+
+            return View(demande);
+        }
+
+        // POST: Demandes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var demande = await _context.Demandes.FindAsync(id);
+            _context.Demandes.Remove(demande);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DemandeExists(int id)
+        {
+            return _context.Demandes.Any(e => e.DemandeID == id);
+        }
+    }
+}
