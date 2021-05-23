da18abfb2be018e0521872f40fbef2193a9df5e2
Jebabra Abdelhakim <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-05-23 22:32:51 +0100

    Add files via upload

---
+++ Migrations/20210523211221_DemandeModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity1.Migrations
+{
+    public partial class DemandeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Demande",
+                columns: table => new
+                {
+                    DemandeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_stage = table.Column<int>(nullable: false),
+                    ID_student = table.Column<int>(nullable: false),
+                    etat = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Demande", x => x.DemandeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Demande");
+        }
+    }
+}
