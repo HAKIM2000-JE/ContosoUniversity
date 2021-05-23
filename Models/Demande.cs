7ef66965591934d87847c9443c1dbb8c00d3a713
Jebabra Abdelhakim <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-05-23 22:31:23 +0100

    Add files via upload

---
+++ Models/Demande.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity1.Models
+{
+    public class Demande
+    {
+
+        public int  DemandeID {get ; set; }
+        public int ID_stage { get; set; }
+        public int ID_student { get; set; }
+        public string  etat { get; set; }
+    }
+}
